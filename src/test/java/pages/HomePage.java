0fc214f73978978cb558c75f990365dc5654a50c
szopen40 <jakub.baranowski1@gmail.com>
2018-05-21 17:59:47 +0200

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,34 +3,34 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.ClickAction;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import utils.AbstractPage;
-
-public class HomePage extends AbstractPage {
+public class HomePage extends StartPage {
 	
 	public HomePage(WebDriver driver){
 		super(driver);
+		PageFactory.initElements(driver, this);
 	}
 
+	@FindBy(id = "nav-link-shopall")
+	private WebElement department;
+	
+	@FindBy(xpath = "//*[@id='a-page']//*[contains(@href,'Camera')]")
+	private WebElement category;
+
 	public HomePage searchItem() {
-		driver.findElement(By.id("nav-link-shopall")).click();
-		//WebDriverWait wait = new WebDriverWait(driver, 10);
-		//WebElement element = wait.until(ExpectedConditions.elementToBeClickable((By.xpath("//*[contains(@href,'Camera')]"))));
-//		System.out.println("Going through categories");
+		department.click();
 		return new HomePage(driver);
 	}
 	public SearchPage chooseCategory(){
-		//WebElement element = driver.findElement(By.partialLinkText("Camera, Photo & Video"));
-		WebElement element = driver.findElement(By.xpath("//*[@id='a-page']//*[contains(@href,'Camera')]"));
-		WebDriverWait wait = new WebDriverWait(driver, 20); //here, wait time is 20 seconds
-
-		wait.until(ExpectedConditions.visibilityOf(element)); //this will wait for elememt to be visible for 20 seconds
-		element.click(); //now it clicks on element
-		
-		//driver.findElement(By.partialLinkText("Camera")).click();
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.visibilityOf(category)); 
+		category.click();
 		return new SearchPage(driver);
 	}
-
 }
