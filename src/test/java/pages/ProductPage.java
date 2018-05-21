11a3c48c71292965017647fd4babd7d0d455848d
szopen40 <jakub.baranowski1@gmail.com>
2018-05-21 18:10:00 +0200

    Update ProductPage.java

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -7,34 +7,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.FindBy;
+
 
 import utils.AbstractPage;
 
-public class ProductPage extends AbstractPage {
+public class ProductPage extends StartPage {
 
 	public ProductPage(WebDriver driver) {
 		super(driver);
+		PageFactory.initElements(driver, this);
 	}
+	
+	@FindBy(name = "quantity")
+	private WebElement quantity;
 
+	@FindBy(xpath = "//input[@id='add-to-cart-button']")
+	private WebElement addToCart;
+	
 	public ProductPage selectQuantity() {
-		WebElement dropdown = driver.findElement(By.name("quantity"));
-		Select drop = new Select(dropdown);
+		Select drop = new Select(quantity);
 		drop.selectByValue("2");
 		return new ProductPage(driver);
 	}
 	
 	public ProductPage additionalAssurance(){
-		// in case of additional assurance offer
 		if (driver.findElement(By.id("siNoCoverage-announce")).isEnabled()) {
-			driver.findElement(By.id("siNoCoverage-announce")).click();
+		   driver.findElement(By.id("siNoCoverage-announce")).click();
 		} else {
-			System.out.println("No additional assurance taken");
+		   System.out.println("No additional assurance taken");
 		}
 		return new ProductPage(driver);
 	}
 
 	public CartPage addToCart() {
-		driver.findElement(By.xpath("//input[@id='add-to-cart-button']")).click();
+		addToCart.click();
 		return new CartPage(driver);
 	}
 }
