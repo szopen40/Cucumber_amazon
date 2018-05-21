5ec503da0522d9436365013ef2e6b4444777d3ad
szopen40 <jakub.baranowski1@gmail.com>
2018-05-21 13:37:34 +0200

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,13 +1,12 @@
 package runner;
 import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "feature", 
-		glue = "runner",
+		features = "src/test/resources/feature", 
+		glue = "stepdef",
 		plugin ={"html:target/cucumber-html-report","json:target/cucumber.json",
 				"pretty:target/cucumber-pretty.txt","usage:/target/cucumber-usage.json",
 				"junit:target/cucumber-results.xml"}
